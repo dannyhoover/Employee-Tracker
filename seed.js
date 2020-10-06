58020aa255418740a4981b818bc49971b8f355d3
dannyhoover <dofjrtd@gmail.com>
2020-10-06 00:07:09 -0700

    seed update

--- seed.js
+++ seed.js
@@ -5,6 +5,8 @@ const orm = require("./orm");
   await orm.createDepartment({name: "HR"});
   await orm.createDepartment({name: "IT"});
   const [hr, it] = await orm.getDepartments();
+  const []
   await orm.createRole({title:"Relations Manager", salary: 20000, departmentId: hr.id});
   await orm.createRole({title:"Tech Support Specialist", salary: 40000, departmentId: it.id});
+  await orm.createEmployee({firstName:"Danny", lastName:"Hoover", roleId: it.id, managerId: role.id})
 })();
